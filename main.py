fa5f2c6a8dc903e7dfdaa068084ae3c93d7ff2a7
Daredevil46201 <eugeny.kuroptev@yandex.ru>
2026-07-03 12:20:44 +0300

    Added language filter and IMDB output

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from core import user_choice
 from wheel import spin_wheel
-from api_client import get_genres, get_random_movie_list
+from api_client import get_genres, get_random_movie_list, get_IMDB
 from datetime import date
 
 def ask_year():
@@ -53,6 +53,30 @@ def ask_genres(film_tv):
     else:
         return set()
 
+def ask_language():
+    print("Вам нужно выбрать оригинальные языки стран для фильмов/сериалов \n'Оригинальные языки стран подразумевают под собой - Язык какой страны распространён у снимавших этот фильм/сериал'")
+    print("Если страна нравится нажмите - '1', если хотите её убрать нажмите - '0'")
+    languages = {"en": "Английский", "ru": "Русский", "ja": "Японский", "ko": "Корейский", "fr":"Французский", "de":"Немецкий", "es":"Испанский", "it":"Итальянский", "zh":"Китайский", "hi":"Индийский", "da":"Датский"}
+    free_choices = (0,1)
+    user_language = set()
+    for k,v in languages.items():
+        while True:
+            try:
+                my_choice = int(input(f'{v}:'))
+                if my_choice in free_choices:
+                    break
+                else:
+                    print("Введите '1' или '0'")
+                    continue
+            except ValueError:
+                print("Неверный тип данных, попробуйте ещё раз")
+        if my_choice:
+            user_language.add(k)
+    if user_language:
+        return user_language
+    else:
+        return None
+    
 def main():
     print("Добро пожаловать! Вам предлагается выбрать фильм/сериал на вечер")
     
@@ -85,29 +109,40 @@ def main():
     player_rate = []
     player_year = []
     no_player_genre = {}
+    player_language = {}
     
     for i in players:
         print(f"Очередь пользователя №{i}:")
         player_rate.append(ask_rate())
         player_year.append(ask_year())
         no_player_genre[i] = ask_genres(film_tv)
+        player_language[i] = ask_language()
+    
+    if None in player_language.values():
+        print("\nУвы один из вас не хочет смотреть ни одного фильма/сериала на популярном человеческом языке, поэтому мы ничего не сможем найти. Смотрим Уральские пельмени")
+        sys.exit()
     
     loosers = set.union(*no_player_genre.values())
     
     if len(loosers) == len(get_genres(film_tv)):
-        print("\nУвы один из вас не хочет смотреть ни одного жанра, поэтому мы не сможем найти ни одного фильма. Смотрим Уральские пельмени")
+        print("\nУвы один из вас не хочет смотреть ни одного жанра, поэтому мы ничего не сможем найти. Смотрим Уральские пельмени")
         sys.exit()
     
     rate = max(player_rate)
     year = max(player_year)
     no_genre = ",".join(map(str,loosers))
+    language = "|".join(set.intersection(*player_language.values()))
+    
+    if not language:
+        print("К сожалению не смогли найти пересечений по выбранным языкам. Смотрим Уральские пельмени")
+        sys.exit()
     
     print("Если фильм/сериал нравится - нажмите '1', если не нравится - нажмите '0' \nДля остановки выбора фильма/сериала наберите - 'стоп' \n")
     
     players_choices = {}
     
     print("Загружаем фильмы... Пожалуйста подождите, это может занять некоторое время")
-    users_data = get_random_movie_list(rate,year,no_genre,film_tv)
+    users_data = get_random_movie_list(rate,year,no_genre,language,film_tv)
     
     if not users_data:
         print("Непредвиденные обстоятельства, мы не смогли найти ни одного фильма/сериала, простите, мы закрываемся")
@@ -121,9 +156,13 @@ def main():
         print(f"Выбор пользователя №{k}: {title}")
     
     winners = set.intersection(*players_choices.values())
+    print(winners)
 
     if len(winners) == 1:
-        print(f"\nФильм победитель на вечер {winners}")
+        winners, = winners
+        print(f"\nПобедитель на вечер - {winners[0]}")
+        print("Если желаете, можете перейти на страницу фильма/сериала")
+        print(get_IMDB(str(winners[1])))
     elif len(winners) > 1:
         print("Добро пожаловать в колесо фортуны \nВыберите режим игры: \nПервый победитель - '1' | Русская рулетка - '0'")
         correct_values = (0,1)
@@ -137,7 +176,10 @@ def main():
                     continue
             except ValueError:
                 print("Ошибка. Допустимые значения: Первый победитель - '1' | Русская рулетка - '0'")
-        print(spin_wheel(winners, game_mode))
+        absolute_winner = spin_wheel(winners, game_mode)
+        print(f"\nПобедитель на вечер - {absolute_winner[0]}")
+        print("Если желаете, можете перейти на страницу фильма/сериала")
+        print(get_IMDB(str(absolute_winner[1])))
     else:
         print("\nУвы совпадений нет. Смотрим Уральские пельмени")
 
